86539860540dd7fde9766ca99d8ea7fbb8ded68e
Seth Penna <spenna@lutron.com>
2018-10-12 14:14:15 -0400

    Database commit
    - Added a database to the app file
    
    - Removed all mention of the data files to be replaced with the database
    
    - Added logic to add and remove users and chores from the database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, abort, jsonify, url_for, redirect
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 import os, time, json, traceback
 import logging, logging.handlers
 import random as random
@@ -19,7 +20,7 @@ external = False
 emailFlag = False
 
 # Location for event
-eventLocation = '45 Westland Ave'
+eventLocation = 'ADDRESS'
 #############################################
 #############################################
 
@@ -43,90 +44,61 @@ flaskLogger.addHandler(fh)
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading')
 
-# Setup global variable to hold chores and users
-global chores
-global users
-usersFileName = 'users.npy'
-choresFileName = 'chores.npy'
-if os.path.isfile(choresFileName): 
-	chores = list(np.load(choresFileName))
-	logger.warning('Using existing chores file')
-else:
-	chores = []
-	np.save(choresFileName, np.array(chores))
-	logger.warning('Creating blank chores file')
+# Setup database
+db_uri = 'sqlite:///{}'.format(os.path.join(os.path.dirname(__file__), 'chores.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+#### DATABASE CLASSES
+# Parent Class
+class User(db.Model):
+	__tablename__ = 'user'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(), unique=False)
+	email = db.Column(db.String(), unique=True)
+	chores = db.relationship('Chore')
 	
-if os.path.isfile(usersFileName): 
-	users = list(np.load(usersFileName))
-	logger.warning('Using existing users file')
-else:
-	users = []
-	np.save(usersFileName, np.array(users))
-	logger.warning('Creating blank users file')
+# Child Class
+class Chore(db.Model):
+	__tablename__ = 'chore'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(), unique=True)
+	frequency = db.Column(db.Integer, unique=False)
+	complete = db.Column(db.Boolean, unique=False)
+	user_id = db.Column(db.Integer, ForeignKey('user.id'))
+
+# Create all
+db.create_all()
+
 
 	
 #####################################################
 # SETUP THE CHORE FUNCTION CALLS
-# Define a function to set the chores 
-# Each user is the key to a dictionary
-# Each value is a list with their chores
-global assignment 
-global choreCompletion
-choreCompletion = {}
-
-assignmentFileName = 'assignment.npy'
-if os.path.isfile(assignmentFileName): 
-	assignment = np.load(assignmentFileName).item()
-	logger.warning('Using existing assignments file')
-	logger.debug(assignment)
-	# Reset completion chart
-	for key,value in assignment.items():
-		choreCompletion[key] = False
-	logger.info('Reset completion')
-else:
-	assignment = {}
-	np.save(assignmentFileName, assignment)
-	logger.warning('Creating blank assignments file')
-	   
+	  
 def initialAssign():
-	global users, chores, assignment, choreCompletion  # Get the global users and chores lists
-	assignment = {} # Reset since it is being written from scratch
-	numUsers = len(users) # Find how many users there are
-	numChores = len(chores) # Find how many chores there are
+	# Get length of lists
+	usersLen = db.session.query(User).count()
+	choreLen = db.session.query(Chore).count()
 	
-	if(numUsers > 0 and numChores > 0):
-		# Create an array for each user in the assignments dictionary
-		for user in users:
-			assignment[user] = []
-
-		# Fill the arrays with the chores
+	# Make sure neither list is empty
+	if(usersLen > 0 and choreLen > 0):
+		# Go through each chore and assign a user to it
 		i = 0
-		for chore in chores:
-			assignment[users[i]].append(chore)
-			i = i + 1
-			if(i >= numUsers):
-				i = 0
-
-		#logger.debug(assignment)
-		np.save(assignmentFileName, assignment)
+		for chore in db.session.query(Chore).all():
+			chore.
+		
 	else:
 		logger.warning('Empty chore list or user list')
 		
 	# Reset completion chart
-	for key,value in assignment.items():
-		choreCompletion[key] = False
+	for chore in db.session.query(Chore).all():
+		chore.complete = False
+	db.session.commit()
+	
 	logger.info('Reset completion')
-
-global emails
-emailsFileName = 'emails.npy'
-if os.path.isfile(emailsFileName): 
-	emails = np.load(emailsFileName).item()
-	logger.warning('Using existing emails file')
-	logger.debug(emails)
-else:
-	emails = {}
-	np.save(emailsFileName, np.array(emails))
-	logger.warning('Creating blank emails file')
 	
 def emailAssignments():  
 	if(emailFlag):
@@ -149,7 +121,7 @@ def emailAssignments():
 		
 		
 def rotateAssign():
-	global assignment, users, chores, choreCompletion # Get the global assignment, user list, and chores list
+	global assignment # Get the global assignment, user list, and chores list
 	numUsers = len(users) # Find how many users there are
 	
 	if(assignment != {}):
@@ -204,7 +176,7 @@ s.add_job(rotateAssign, 'cron', id='rotation', day_of_week='wed,sat', hour='23',
 # Define a landing page
 @app.route('/', methods=['POST', 'GET'])
 def home():
-	global assignment, users, chores, choreCompletion # Get all global variables
+	global assignment # Get all global variables
 	warnings = ''
 	
 	# Find which day the current assignments are due
@@ -221,22 +193,20 @@ def home():
 		return render_template('index.html', tableValues=assignment, warnings=warnings, endDate=endDate)
 	
 	# Return the table
-	return render_template('index.html', tableValues=assignment, warnings=warnings, endDate=endDate, completion=choreCompletion)
+	return render_template('index.html', tableValues=assignment, warnings=warnings, endDate=endDate)
 
 	
 # When the user completes a chore
 @socketio.on('chorecomplete', namespace='/chore')
 def choreComplete(chore):
-	global choreCompletion
 	try:
-		choreCompletion[chore] = True
-		logger.info('Chore ' + str(chore) + ' complete!')
-		socketio.emit('update', namespace='/chore')
+		chore = db.session.query(Chore).filter(Chore.name == chore)
+		chore.complete = True
 	except:
-		logger.error("Couldn't complete chore " + str(chore))
 		logger.error(traceback.print_exc())
 	
-	
+
+### Server error handling	
 # Load a simple page on 404 error which directs user back to safety at home page
 @app.errorhandler(404)
 def errorHandle(e):
@@ -244,11 +214,23 @@ def errorHandle(e):
 		logger.warning('User attempted to navigate to ' + str(request.url))
 	return render_template('404.html')
 
+@socketio.on_error()
+def error_handler(e):
+	logger.critical('Socket IO Error')
+	logger.critical(traceback.print_exc())
+	logger.critical(e)
+
+@socketio.on_error_default
+def default_error_handler(e):
+	logger.critical('Socket IO Error')
+	logger.critical(traceback.print_exc())
+	logger.critical(e)
+	
 	
 # Define a maintenance/setup page
 @app.route('/setup', methods=['POST', 'GET'])
 def setup():
-	global chores, users, assignment, emails # Get global chores list and users list
+	global assignment # Get global chores list and users list
 	message = '' # Reset message field
 	changeFlag = 0
 	if request.method == 'POST':
@@ -256,10 +238,11 @@ def setup():
 		if 'addChore' in request.form:
 			changeFlag = 1
 			addValue = request.form['addvalue']
-			if(addValue != '' and addValue not in chores):
+			if(addValue != ''):
 				logger.warning('Adding chore: ' + str(addValue))
-				chores.append(addValue)
-				np.save(choresFileName, np.array(chores))
+				chore = Chore(name=addValue, frequency=0, complete=False)
+				db.session.add(chore)
+				db.session.commit()
 				message = 'Added ' + str(addValue) + ' to list'
 			
 		elif 'removeChore' in request.form:
@@ -267,8 +250,7 @@ def setup():
 			removeValue = request.form['removevalue']
 			logger.warning('Removing chore: ' + str(removeValue))
 			try:
-				chores.remove(removeValue)
-				np.save(choresFileName, np.array(chores))
+				db.session.query(Chore).filter(Chore.name == removeValue).remove()
 				message = 'Removed ' + str(removeValue) + ' from list'
 			except:
 				logger.error('Could not remove chore, does not exist')
@@ -278,12 +260,11 @@ def setup():
 			changeFlag = 1
 			addValue = request.form['addUserValue']
 			emailValue = request.form['userEmail']
-			if(addValue != '' and addValue not in users):
+			if(addValue != ''):
 				logger.warning('Adding user: ' + str(addValue))
-				users.append(addValue)
-				emails[addValue] = str(emailValue)
-				np.save(emailsFileName, emails)
-				np.save(usersFileName, np.array(users))
+				user = User(name=addValue, email=emailValue)
+				db.session.add(user)
+				db.session.commit()
 				message = 'Added ' + str(addValue) + ' from list'
 				
 		elif 'removeUser' in request.form:
@@ -291,8 +272,7 @@ def setup():
 			removeValue = request.form['removeUserValue']
 			logger.warning('Removing user: ' + str(removeValue))
 			try:
-				users.remove(removeValue)
-				np.save(usersFileName, np.array(users))
+				db.session.query(User).filter(User.name == removeValue).remove()
 				message = 'Removed ' + str(removeValue) + ' from list'
 			except:
 				logger.error('Could not remove user, does not exist')
@@ -313,28 +293,25 @@ def setup():
 			
 		elif 'clear' in request.form:
 			if(request.form['clearConfirm'] == 'clear'):
-				users = []
-				chores = []
-				assignment = {}
-				emails = {}
-				np.save(usersFileName, np.array(users))
-				np.save(choresFileName, np.array(chores))
-				np.save(assignmentFileName, assignment)
-				np.save(emailsFileName, emails)
+				db.session.query(User).all().remove()
+				db.session.query(Chore).all().remove()
 				logger.warning('All data cleared')
 			else:
 				logger.warning('Type "clear" to confirm and repress')
 			
 		if(changeFlag):
-			random.shuffle(chores)
-			#logger.debug(chores)
-			initialAssign() # If any change has been made the chores need to be re-assigned
+			pass
+			#random.shuffle()
+			#initialAssign() # If any change has been made the chores need to be re-assigned
 				
 	try:
 		nextrun = s.get_jobs()[0].next_run_time
 	except Exception as e:
 		nextrun = "Couldn't get next runtime " + e
 	
+	chores = db.session.query(Chore.name).all()
+	users = db.session.query(User.name).all()
+	emails = db.session.query(User.email).all()
 	return render_template('setup.html', chores=chores, message=message, users=users, assignment=assignment, nextrun=nextrun, emails=emails)
 
 
